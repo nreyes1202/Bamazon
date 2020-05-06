13124d9efc60c56bc97a4d1218672e64595568aa
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-05 22:43:17 -0600

    created connection to mysql in js file

--- customers/customers.js
+++ customers/customers.js
@@ -2,3 +2,15 @@ const inquirer = require("inquirer")
 const mysql = require("mysql")
 require("console.table")
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'Bamazon'
+  });
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
+
+  
