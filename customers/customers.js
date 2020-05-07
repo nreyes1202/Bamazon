8d019c6ece9580e0dbc21ae251b8a2b16d6d98ec
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-07 13:42:36 -0600

    added validation to prompt

--- customers/customers.js
+++ customers/customers.js
@@ -2,79 +2,77 @@ const inquirer = require("inquirer")
 const mysql = require("mysql")
 require("console.table")
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'Bamazon'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+  main()
+
+});
+
 const main = () => {
-    // const database = connectToDB()
-    // let inventory = loadProducts(database)
-    // askCustomerForItem(inventory)
   loadProducts()
 }
 
-// const connectToDB = () => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'Bamazon'
-      });
-      connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected!');
-        main()
-    
-      });
-//       return connection
-// }
+const loadProducts = db => {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err
+    console.table(res)
+    askCustomerForItem()
+  })
+}
 
-//   function loadProducts(){
+const askCustomerForItem = items => {
+  // The first should ask them the ID of the product they would like to buy.
+  inquirer.prompt({
+    //Validate: val => //logic to validate !isNaN
+    type: "input",
+    name: "id",
+    message: "What item would you like to purchase?",
+    validate: function(value) {
+      if(!isNaN(value)) {
+        return true
+      }
+      return "Sorry! Not valid!"
+    }
 
-//   }
-  const loadProducts = db => {
-    connection.query("SELECT * FROM products", (err, res) => {
-      if (err) throw err
-      console.table(res)
-      askCustomerForItem()
-    })
-  }
+  }).then(val => {
+    //TO DO: Check if value exists.
+    // Check if there is more than 0 quantity.
+    //TO DO: return the product for next function to use.
 
-  const askCustomerForItem = items => {
-    // The first should ask them the ID of the product they would like to buy.
-    inquirer.prompt({
-        //TO DO: Look up inquirer.promp documentation.
-        //Validate: val => //logic to validate !isNaN
-      type: "input",
-      name: "id",
-      message: "What item would you like to purchase?"
 
-    }).then(val => {
-        //TO DO: Check if value exists.
-        // Check if there is more than 0 quantity.
-        //TO DO: return the product for next function to use.
-        console.log(val)
-    }) 
-  }
+    console.log(val)
+  })
+}
 
-  const getCustomerQuantity = item => {
-    // The second message should ask how many units of the product they would like to buy.
-    //TO DO: write similar logic to inquirer.prompt but for quantity instead of item.
-    //Return the item and quantity as a single array.
-  }
+const getCustomerQuantity = item => {
+  // The second message should ask how many units of the product they would like to buy.
+  //TO DO: write similar logic to inquirer.prompt but for quantity instead of item.
+  //Return the item and quantity as a single array.
+}
 
-  const isItemAvailable = (itemCustomerChose, inventory) => {
-    // Once the customer has placed the order, 
-    // your application should check if your store has enough of the product to meet the customer's request.
-    // If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-    //TO DO: return true or false.
-  }
+const isItemAvailable = (itemCustomerChose, inventory) => {
+  // Once the customer has placed the order, 
+  // your application should check if your store has enough of the product to meet the customer's request.
+  // If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+  //TO DO: return true or false.
+}
 
-  const customerCheckoutCart = (itemCustomerWillBuy, quantity) => {
-//     However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
-    //
-  }
+const customerCheckoutCart = (itemCustomerWillBuy, quantity) => {
+  //     However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+  //    * This means updating the SQL database to reflect the remaining quantity.
+  //    * Once the update goes through, show the customer the total cost of their purchase.
+  //
+}
 
-  const leaveStore = () => {
+const leaveStore = () => {
 
-  }
+}
 
   //Manager folder -> add items to inventory
