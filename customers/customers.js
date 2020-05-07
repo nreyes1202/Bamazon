8f3bf576faf40630da884c318ef213a5cc828960
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-07 15:39:49 -0600

    added function to display total price and added pseudocode

--- customers/customers.js
+++ customers/customers.js
@@ -1,6 +1,10 @@
+//Requiring inquirer, Mysql and console.table to display "store."
+
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 require("console.table")
+
+//Creating connection to Mysql database.
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -15,10 +19,13 @@ connection.connect((err) => {
 
 });
 
+//Main function to display all products on Bamazon.
+
 const main = () => {
   loadProducts()
 }
 
+//Creating this function to load all products from database when node is run.
 const loadProducts = db => {
   connection.query("SELECT * FROM products", (err, res) => {
     if (err) throw err
@@ -56,12 +63,13 @@ const askCustomerForItem = items => {
       // console.table(res)
       // TO DO: check to see if there is enough in stock from res
       const stockQuantity = res[0].stock_quantity
+      const priceOfCart = res[0].price * userQuantity
       console.log(res)
       console.log(stockQuantity)
       if (userQuantity <= stockQuantity) {
         console.log("We have enough in stock!")
-        var newQuantity = stockQuantity - userQuantity
-        customerCheckoutCart(val.id, newQuantity)
+        const newQuantity = stockQuantity - userQuantity
+        customerCheckoutCart(val.id, newQuantity, priceOfCart)
       }
       else {
         console.log("Insufficient quantity!")
@@ -73,17 +81,16 @@ const askCustomerForItem = items => {
 }
 
 
-// const isItemAvailable = (itemCustomerChose, inventory) => {
+
 //   // Once the customer has placed the order, 
 //   // your application should check if your store has enough of the product to meet the customer's request.
 //   // If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-// }
 
-const customerCheckoutCart = (id, quantity) => {
+const customerCheckoutCart = (id, quantity, price) => {
   //     However, if your store _does_ have enough of the product, you should fulfill the customer's order.
   //    * This means updating the SQL database to reflect the remaining quantity.
   //    * Once the update goes through, show the customer the total cost of their purchase.
-  console.log(id, quantity)
+  console.log(id, quantity, price)
   connection.query("UPDATE products SET ? WHERE ?", [
     {stock_quantity: quantity},
     {item_id: id}
@@ -91,14 +98,16 @@ const customerCheckoutCart = (id, quantity) => {
     function (error) {
       if (error) throw err;
       console.log("Order placed successfully!");
-      
-      main();
+      console.log("The total price is $" + price)
+      main(); //instead of callng main, do another inquirer and ask "would you like to purchase anoter item"
+      //if they want to leave, then execute leavestore
+      //
     }
   )
 }
 
 const leaveStore = () => {
-
+// look up connection.end
 }
 
   //Manager folder -> add items to inventory
